b131a174a186d5a4bef1c0211725ff7add1014e2
qinyouj <1784885676@qq.com>
2021-09-14 00:48:55 +0800

    deploy

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home-container">
+    <router-view></router-view>
+    <TabBar />
+  </div>
+</template>
+
+<script>
+import TabBar from '@/components/tabBar.vue';
+
+export default {
+  components: {
+    TabBar,
+  },
+};
+</script>
+
+<style scoped>
+.home-container{
+  width: 100%;
+  height: 100%;
+  overflow-y: hidden;
+}
+</style>
